cb4d99970fa3cdb3ab5b03fd6df31de83d39ff1c
Minh Quan <doquan19062004@gmail.com>
2026-05-10 15:18:43 +0700

    chore: delete folder backendd (unused)

--- backendd/start.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8888 -w 4 app.main:app
--- backendd/tests/test_health.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_health(test_client):
-    resp = test_client.get('/health')
-    assert 200 == resp.status_code
